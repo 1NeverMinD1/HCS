289d2bca691bb59a2ece0822365649587bc19beb
NeverMinD <nurumbaevdias05@gmail.com>
2026-06-03 22:17:29 +0500

    styleandBlogsChanges

--- frontend/src/components/pages/blogs/BlogsPageBlocks/BlogsPageBlocks.jsx
+++ frontend/src/components/pages/blogs/BlogsPageBlocks/BlogsPageBlocks.jsx
@@ -1,13 +1,16 @@
-import BlogsBlock from "./BlogsPageBlock/BlogsPageBlock";
+import BlogsPageBlock from "./BlogsPageBlock/BlogsPageBlock";
 import BlogsPageFirstBlock from "./BlogsPageFirstBlock/BlogsPageFirstBlock";
 
 export default function BlogsPageBlocks({ blogs }) {
+  const blog = blogs[0];
   return (
-    <div className="blogspge__blocks">
-      <BlogsPageFirstBlock blogs={item} />
-      {blogs.map((item) => (
-        <BlogsBlock key={item.id} blog={item} />
-      ))}
+    <div className="blogspage__blocks">
+      <BlogsPageFirstBlock blog={blog} />
+      <div className="blogspage__list">
+        {blogs.slice(1).map((item) => (
+          <BlogsPageBlock key={item.id} blog={item} />
+        ))}
+      </div>
     </div>
   );
 }
