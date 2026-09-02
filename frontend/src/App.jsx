700d77677f51c1d308b9d82c46abe1d44bf36ff2
NeverMinD <nurumbaevdias05@gmail.com>
2026-09-03 02:04:30 +0500

    PageSpeedLazyCSSAdd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import { LocaleProvider } from "./context/LocaleContext.jsx";
@@ -7,32 +7,54 @@ import { useYandexMetrika } from "./utils/useYandexMetrika.js";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
-import Home from "./components/pages/Home.jsx";
-import NewsPage from "./components/pages/news/NewsPage.jsx";
-import ArtsPage from "./components/pages/articles/ArtsPage.jsx";
-import BlogsPage from "./components/pages/blogs/BlogsPage.jsx";
-import EventsPage from "./components/pages/events/EventsPage.jsx";
-import QNA from "./components/pages/qna/QNA.jsx";
+const Home = lazy(() => import("./components/pages/Home.jsx"));
+const NewsPage = lazy(() => import("./components/pages/news/NewsPage.jsx"));
+const ArtsPage = lazy(() => import("./components/pages/articles/ArtsPage.jsx"));
+const BlogsPage = lazy(() => import("./components/pages/blogs/BlogsPage.jsx"));
+const EventsPage = lazy(
+  () => import("./components/pages/events/EventsPage.jsx"),
+);
+const QNA = lazy(() => import("./components/pages/qna/QNA.jsx"));
 
-import NewsContent from "./components/pages/contentpages/news/NewsContent.jsx";
-import ArticlesContent from "./components/pages/contentpages/articles/ArticlesContent.jsx";
-import BlogsContent from "./components/pages/contentpages/blogs/BlogsContent.jsx";
-import EventsContent from "./components/pages/contentpages/events/EventsContent.jsx";
-import QnasContent from "./components/pages/contentpages/qnas/QnasContent.jsx";
+const NewsContent = lazy(
+  () => import("./components/pages/contentpages/news/NewsContent.jsx"),
+);
+const ArticlesContent = lazy(
+  () => import("./components/pages/contentpages/articles/ArticlesContent.jsx"),
+);
+const BlogsContent = lazy(
+  () => import("./components/pages/contentpages/blogs/BlogsContent.jsx"),
+);
+const EventsContent = lazy(
+  () => import("./components/pages/contentpages/events/EventsContent.jsx"),
+);
+const QnasContent = lazy(
+  () => import("./components/pages/contentpages/qnas/QnasContent.jsx"),
+);
 
-import Authors from "./components/pages/contentpages/authors/Authors.jsx";
-import CategoryPage from "./components/pages/contentpages/сategoryPage/CategoryPage.jsx";
-import TagPage from "./components/pages/contentpages/tagPage/TagPage.jsx";
+const Authors = lazy(
+  () => import("./components/pages/contentpages/authors/Authors.jsx"),
+);
+const CategoryPage = lazy(
+  () => import("./components/pages/contentpages/сategoryPage/CategoryPage.jsx"),
+);
+const TagPage = lazy(
+  () => import("./components/pages/contentpages/tagPage/TagPage.jsx"),
+);
 
 // FooterLinks
 
-import Imprint from "./components/footer-links/Imprint.jsx";
-import About from "./components/footer-links/About.jsx";
-import Advertising from "./components/footer-links/Advertising.jsx";
-import Contacts from "./components/footer-links/Contacts.jsx";
-import EditorialPolicy from "./components/footer-links/Editorial-policy.jsx";
-import Privacy from "./components/footer-links/Privacy.jsx";
-import Terms from "./components/footer-links/Terms.jsx";
+const Imprint = lazy(() => import("./components/footer-links/Imprint.jsx"));
+const About = lazy(() => import("./components/footer-links/About.jsx"));
+const Advertising = lazy(
+  () => import("./components/footer-links/Advertising.jsx"),
+);
+const Contacts = lazy(() => import("./components/footer-links/Contacts.jsx"));
+const EditorialPolicy = lazy(
+  () => import("./components/footer-links/Editorial-policy.jsx"),
+);
+const Privacy = lazy(() => import("./components/footer-links/Privacy.jsx"));
+const Terms = lazy(() => import("./components/footer-links/Terms.jsx"));
 
 export default function App() {
   useYandexMetrika();
@@ -49,59 +71,65 @@ export default function App() {
       <div className="main">
         <Header />
 
-        <Routes>
-          <Route path="/" element={<Navigate to="/ru" replace />} />
-          {/* Домашняя страница */}
-          <Route path="/:locale" element={<Home />} />
-          {/* Все новости */}
-          <Route path="/:locale/news" element={<NewsPage />} />
-          {/* Главные новости */}
-          <Route path="/:locale/news/main" element={<NewsPage />} />
-          {/* Категории новостей */}
-          <Route path="/:locale/news/category/:id" element={<NewsPage />} />
-          {/* Полная новость */}
-          <Route path="/:locale/news/:slug" element={<NewsContent />} />
-          {/* Все статьи */}
-          <Route path="/:locale/articles" element={<ArtsPage />} />
-          {/* Полная статья */}
-          <Route path="/:locale/articles/:slug" element={<ArticlesContent />} />
-          {/* Все блоги */}
-          <Route path="/:locale/blogs" element={<BlogsPage />} />
-          {/* Полный блог */}
-          <Route path="/:locale/blogs/:slug" element={<BlogsContent />} />
-          {/* Все мероприятия */}
-          <Route path="/:locale/events" element={<EventsPage />} />
-          {/* Полное мероприятие */}
-          <Route path="/:locale/events/:slug" element={<EventsContent />} />
-          {/* Вопросы и ответы */}
-          <Route path="/:locale/q-and-as" element={<QNA />} />
-          {/* Полный вопрос */}
-          <Route path="/:locale/q-and-as/:slug" element={<QnasContent />} />
-          {/* Автор */}
-          <Route path="/:locale/author/:slug" element={<Authors />} />
-          {/* FooterLinks */}
-          {/* Imprint */}
-          <Route path="/:locale/imprint" element={<Imprint />} />
-          {/* About */}
-          <Route path="/:locale/about" element={<About />} />
-          {/* Advertising */}
-          <Route path="/:locale/advertising" element={<Advertising />} />
-          {/* Contacts */}
-          <Route path="/:locale/contacts" element={<Contacts />} />
-          {/* Editorial-policy */}
-          <Route
-            path="/:locale/editorial-policy"
-            element={<EditorialPolicy />}
-          />
-          {/* Privacy */}
-          <Route path="/:locale/privacy" element={<Privacy />} />
-          {/* Terms */}
-          <Route path="/:locale/terms" element={<Terms />} />
-          {/* Категория (для статей/блогов/событий/qna) */}
-          <Route path="/:locale/category/:id" element={<CategoryPage />} />
-          {/* Тег */}
-          <Route path="/:locale/tag/:id" element={<TagPage />} />
-        </Routes>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/ru" replace />} />
+            {/* Домашняя страница */}
+            <Route path="/:locale" element={<Home />} />
+            {/* Все новости */}
+            <Route path="/:locale/news" element={<NewsPage />} />
+            {/* Главные новости */}
+            <Route path="/:locale/news/main" element={<NewsPage />} />
+            {/* Категории новостей */}
+            <Route path="/:locale/news/category/:id" element={<NewsPage />} />
+            {/* Полная новость */}
+            <Route path="/:locale/news/:slug" element={<NewsContent />} />
+            {/* Все статьи */}
+            <Route path="/:locale/articles" element={<ArtsPage />} />
+            {/* Полная статья */}
+            <Route
+              path="/:locale/articles/:slug"
+              element={<ArticlesContent />}
+            />
+            {/* Все блоги */}
+            <Route path="/:locale/blogs" element={<BlogsPage />} />
+            {/* Полный блог */}
+            <Route path="/:locale/blogs/:slug" element={<BlogsContent />} />
+            {/* Все мероприятия */}
+            <Route path="/:locale/events" element={<EventsPage />} />
+            {/* Полное мероприятие */}
+            <Route path="/:locale/events/:slug" element={<EventsContent />} />
+            {/* Вопросы и ответы */}
+            <Route path="/:locale/q-and-as" element={<QNA />} />
+            {/* Полный вопрос */}
+            <Route path="/:locale/q-and-as/:slug" element={<QnasContent />} />
+            {/* Автор */}
+            <Route path="/:locale/author/:slug" element={<Authors />} />
+            {/* FooterLinks */}
+            {/* Imprint */}
+            <Route path="/:locale/imprint" element={<Imprint />} />
+            {/* About */}
+            <Route path="/:locale/about" element={<About />} />
+            {/* Advertising */}
+            <Route path="/:locale/advertising" element={<Advertising />} />
+            {/* Contacts */}
+            <Route path="/:locale/contacts" element={<Contacts />} />
+            {/* Editorial-policy */}
+            <Route
+              path="/:locale/editorial-policy"
+              element={<EditorialPolicy />}
+            />
+            {/* Privacy */}
+            <Route path="/:locale/privacy" element={<Privacy />} />
+            {/* Terms */}
+            <Route path="/:locale/terms" element={<Terms />} />
+            {/* Категория (для статей/блогов/событий/qna) */}
+            <Route path="/:locale/category/:id" element={<CategoryPage />} />
+            {/* Тег */}
+            <Route path="/:locale/tag/:id" element={<TagPage />} />
+          </Routes>
+        </Suspense>
+
         <hr className="main_divide" />
         <div className="wrapper">
           <Footer />
