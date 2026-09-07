85e0370781573978e067c94cc726335230dc0e67
NeverMinD <nurumbaevdias05@gmail.com>
2026-09-08 00:26:12 +0500

    ArticlesFetchFix3

--- frontend/src/components/pages/contentpages/articles/ArticlesContent.jsx
+++ frontend/src/components/pages/contentpages/articles/ArticlesContent.jsx
@@ -369,7 +369,10 @@ export default function ArticlesContent() {
     `&populate[tags][fields][0]=name_ru` +
     `&populate[tags][fields][1]=name_kk` +
     `&populate[tags][fields][2]=name_en` +
-    `&populate[scripts]=*`;
+    `&populate[scripts][fields][0]=name` +
+    `&populate[scripts][fields][1]=script_ru` +
+    `&populate[scripts][fields][2]=script_kk` +
+    `&populate[scripts][fields][3]=script_en`;
 
   useEffect(() => {
     setArticlesList([]);
